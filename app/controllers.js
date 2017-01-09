2da13f3a970792e2fb0decd2a89c2aff4888436f
Jacob Elder <jacobelder@Jacobs-MacBook-Pro.local>
2017-01-09 10:56:49 -0800

    showing more weather info and flashing an error message if the city wasnt found

--- app/controllers.js
+++ app/controllers.js
@@ -2,10 +2,10 @@ angular.module("fish-cast")
 .service('searchService', function($http, $state){
 
 	this.location;
-	this.date = Date.now();
-    this.forecast;
+    this.forecast = null;
     this.lat;
     this.lng;
+    this.error = false;
 
     var searchService = this;
 
@@ -16,36 +16,55 @@ angular.module("fish-cast")
             method: 'GET',
             url: 'http://api.wunderground.com/api/8511c7dcba1454f3/forecast10day/q/CA/' + searchService.location + '.json'
         }).then(function successCallback(response) {
-            searchService.forecast = response.data.forecast.simpleforecast.forecastday;
-            $http({
-                method: 'GET',
-                url: 'http://api.wunderground.com/api/8511c7dcba1454f3/conditions/q/CA/' + searchService.location + '.json'
-            }).then(function successCallback(response) {
-                searchService.lat = response.data.current_observation.display_location.latitude;
-                searchService.lng = response.data.current_observation.display_location.longitude;
-                $state.go('results');
-            });
+                console.log(response);
+                if (response.data.response.error) {
+                    searchService.error = true;
+                }
+                else {
+                    searchService.forecast = response.data.forecast.simpleforecast.forecastday;
+                    $http({
+                        method: 'GET',
+                        url: 'http://api.wunderground.com/api/8511c7dcba1454f3/conditions/q/CA/' + searchService.location + '.json'
+                    }).then(function successCallback(response) {
+                        searchService.lat = response.data.current_observation.display_location.latitude;
+                        searchService.lng = response.data.current_observation.display_location.longitude;
+                        searchService.city = response.data.current_observation.display_location.full;
+                        $state.go('results');
+                    });
+                }
         });
 
     }
 
 })
-.controller('landingCtrl', function($scope, searchService) {
-    $scope.date = searchService.date;
+.controller('landingCtrl', function($scope, searchService, $timeout) {
     $scope.location;
-    console.log($scope.date);
+    $scope.error = false;
 
     $scope.search = function(){
         console.log($scope.location);
         searchService.search($scope.location);
+        $timeout(checkForError, 1000);
     }
+
+    function checkForError() {
+        $scope.error = searchService.error;
+        if ($scope.error == true) {
+            $timeout(resetError, 5000);
+        }
+    };
+
+    function resetError() {
+        $scope.error = false;
+    };
 
 })
 .controller('resultsCtrl', function($scope, searchService) {
     console.log("resultsCtrl loaded!");
     $scope.lat = searchService.lat;
     $scope.lng = searchService.lng;
-    $scope.map = { center: { latitude: $scope.lat, longitude: $scope.lng }, zoom: 8 };
+    $scope.city = searchService.city;
+    $scope.map = { center: { latitude: $scope.lat, longitude: $scope.lng }, zoom: 11 };
     $scope.forecast = searchService.forecast;
     console.log($scope.forecast);
 })
